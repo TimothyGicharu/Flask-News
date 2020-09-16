0695245bd9c7db14cfee856a45e61af1bd97a4ee
Timothy Munene <munenegicharu@gmail.com>
2020-09-16 07:44:44 +0300

    Redefine routes using @main

--- app/main/views.py
+++ app/main/views.py
@@ -4,7 +4,7 @@ from app import app
 from ..request import get_source, get_source_articles
 
 # Views
-@app.route('/')
+@main.route('/')
 def index():
     '''
     View root page function that returns the index page and its data
@@ -15,7 +15,7 @@ def index():
     title = 'Home - Top News'
     return render_template('index.html', title = title,  sources = news_source)
 
-@app.route('/sources/<id>')
+@main.route('/sources/<id>')
 def source_articles(id):
     '''
     View news page function that returns the news details and the 
@@ -24,6 +24,6 @@ def source_articles(id):
     articles = get_source_articles(id)
     return render_template('articles.html', articles = articles)
 
-@app.route('/about')
+@main.route('/about')
 def about():
     return render_template('about.html')
